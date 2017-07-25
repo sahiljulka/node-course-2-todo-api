cbbeed5eb43ebf2660eeeca6924dd7d6c4542f5b
Sahil Julka <sahil.julka@nagarro.com>
2017-07-25 11:13:49 +0530

    Chnage in Mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 //mongoose.connect('mongodb://sahil:1234567@ds043200.mlab.com:43200/todoapp');
 
 module.exports={mongoose};
